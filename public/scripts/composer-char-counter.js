e55a1ff63efe4a1d48413754af9e2128493e49dd
vagrant <mikesnow444@gmail.com>
2019-01-22 22:18:30 +0000

    removed unnecessary variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,10 @@ $(document).ready(function() {
   let tweetTextarea = $("main section form textarea");
 
   tweetTextarea.on("input", function(event) {
-    let changedTextarea = $(this);
 
     //select the counter using sibling function
     let textareaCounter = $($(this).siblings("span"));
-    let charsLeft = 140 - changedTextarea.val().length;
+    let charsLeft = 140 - $(this).val().length;
 
     //add class that changes font colour to red if counter is negative
     if(charsLeft < 0){
