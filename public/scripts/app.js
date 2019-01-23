c2cfd7939b07e856794722639ff347f4b75bdff5
vagrant <mikesnow444@gmail.com>
2019-01-23 02:03:48 +0000

    added render function that loops through array of tweets and appends them to app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,8 @@
 $( document ).ready(function() {
 
   //sample tweet object
-  tweetData = {
+  const data = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -21,8 +22,47 @@ $( document ).ready(function() {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
+];
 
+function renderTweets(tweets) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    tweets.forEach((tweet) => {
+      const addTweet = createTweetElement(tweet);
+      $('#tweets-container').append(addTweet);
+    })
+}
 
   //function that takes in tweet and displays it in tweet container
   function createTweetElement(tweetObj) {
@@ -45,10 +85,5 @@ $( document ).ready(function() {
     return article;
   }
 
-  var $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+ renderTweets(data);
 });
