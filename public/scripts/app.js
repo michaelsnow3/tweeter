ae79693035ec096f824e601b15e200aadf749108
vagrant <mikesnow444@gmail.com>
2019-01-23 20:21:41 +0000

    app now prevents XSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,21 +6,29 @@
 
 $( document ).ready(function() {
 
-function renderTweets(tweets) {
-  // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    tweets.forEach((tweet) => {
-      const addTweet = createTweetElement(tweet);
-      $('#tweets-container').prepend(addTweet);
-    })
-}
+  function renderTweets(tweets) {
+    // loops through tweets
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      tweets.forEach((tweet) => {
+        const addTweet = createTweetElement(tweet);
+        $('#tweets-container').prepend(addTweet);
+      })
+  }
+
+  //function used to prevent XSS
+  function escape(str) {
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(str));
+    return p.innerHTML;
+  }
 
   //function that takes in tweet and displays it in tweet container
   function createTweetElement(tweetObj) {
 
     //convert time in miliseconds to date
     const tweetDate = new Date(tweetObj.created_at);
+
     return $(`
       <article class="tweet">
         <header>
@@ -28,7 +36,7 @@ function renderTweets(tweets) {
           <p class="name">${tweetObj.user.name}</p>
           <p class="handle">${tweetObj.user.handle}</p>
         </header>
-        <p class="tweet-content">${tweetObj.content.text}</p>
+          <p class="tweet-content">${escape(tweetObj.content.text)}</p>
         <footer>
           ${tweetDate.toDateString()}
           <img class="icons" src="/images/flag.png">
